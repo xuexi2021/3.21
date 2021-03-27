48e7af346f1ab3f71c367ad7218171b2de40faac
xuexi2021 <78026247+xuexi2021@users.noreply.github.com>
2021-03-27 20:48:07 +0800

    Update jd_get_share_code.js

--- jd_get_share_code.js
+++ jd_get_share_code.js
@@ -10,6 +10,7 @@
 #获取互助码
 20 13 * * 6 https://gitee.com/lxk0301/jd_scripts/raw/master/jd_get_share_code.js, tag=获取互助码, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 
+
 ================Loon==============
 [Script]
 cron "20 13 * * 6" script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_get_share_code.js, tag=获取互助码
